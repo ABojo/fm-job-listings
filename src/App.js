45d18eba5cac2b285597fa1176d2f6b4c1338a50
Alex Bojorquez <Alex.bojorquez2598@gmail.com>
2023-03-07 14:51:51 -0500

    changed job list styling and added it to app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import JobList from "./components/JobList/JobList";
 
 function App() {
   return (
     <>
       <Header />
+      <JobList />
     </>
   );
 }
